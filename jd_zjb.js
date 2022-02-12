9c25fd0efeb25dba43e690b1582da24372e127c1
dafeige0101 <85019304+dafeige0101@users.noreply.github.com>
2022-02-12 10:08:02 +0800

    Update jd_zjb.js

--- jd_zjb.js
+++ jd_zjb.js
@@ -2,7 +2,7 @@
 #极速版赚金币
 ##入口为极速版 百元生活费 赚金币 邀请好友
 ##第一次运行可不填写邀请码 运行一次查看自己的邀请码
-export InviterPin="9vOskAagcMJ4EOWXPQSS9A%3D%3D" ##你的邀请码
+export InviterPin="RSS/CYCV2LZfHzvijsE+Jg==" ##你的邀请码
 ##助力逻辑：填写你的邀请码变量之后会助力你填写的邀请码
 
 
@@ -63,7 +63,7 @@ const JD_API_HOST = 'https://api.m.jd.com/client.action';
       if (InviterPin.length != 0) {
         await help()
       } else {
-        await help2("zjb",Math.random() > 0.5 ? "9vOskAagcMJ4EOWXPQSS9A%3D%3D" : "9irilvenEupYF488TUrl19DLuKQ9zWnXYHf9anC0ujw%3D")        
+        await help2("zjb",Math.random() > 0.5 ? "RSS/CYCV2LZfHzvijsE+Jg==" : "9irilvenEupYF488TUrl19DLuKQ9zWnXYHf9anC0ujw%3D")        
       }
     }
   }
